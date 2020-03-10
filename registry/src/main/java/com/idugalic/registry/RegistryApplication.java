288e6be085040fe10ccab7724640d1c778dd86e1
huang hong fei <vip96300@163.com>
2020-03-10 15:31:45 +0800

    init

--- registry/src/main/java/com/idugalic/registry/RegistryApplication.java
+++ registry/src/main/java/com/idugalic/registry/RegistryApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class RegistryApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(RegistryApplication.class, args);
     }
 }
