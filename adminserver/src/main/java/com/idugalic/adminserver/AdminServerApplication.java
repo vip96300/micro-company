69f6220e7f53c84b8be53c819df0d29ade902ca9
huang hong fei <vip96300@163.com>
2020-03-10 15:44:43 +0800

    init

--- adminserver/src/main/java/com/idugalic/adminserver/AdminServerApplication.java
+++ adminserver/src/main/java/com/idugalic/adminserver/AdminServerApplication.java
@@ -1,4 +1,4 @@
-package com.idugalic.configserver;
+package com.idugalic.adminserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
